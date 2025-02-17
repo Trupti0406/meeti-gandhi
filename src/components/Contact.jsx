685d60a9e46cd8b6a515eb9fe3bb3bf8d9cdea66
Trupti <truptivijayyadav27@gmail.com>
2025-02-17 16:14:16 +0530

    updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -63,6 +63,10 @@ const Contact = () => {
         }
       );
   };
+//  console.log("Public Key:", import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY);
+//  console.log("Service ID:", import.meta.env.VITE_APP_EMAILJS_SERVICE_ID);
+//  console.log("Template ID:", import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID);
+
 
   return (
     <div
